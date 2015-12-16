ed15540edeea454dc8caf70a6aea3b769065b71d
Jack Nelson <janelson@mymail.mines.edu>
2015-12-15 20:00:04 -0700

    changed color of pinball shader to be much brighter

--- reflectiveMapping.f.glsl
+++ reflectiveMapping.f.glsl
@@ -19,6 +19,6 @@ void main(void)
    float inverse_dot_view = 1.0 - max(dot(normalize(n_normal + noise_vector), n_wiew_direction), 0.0);
    vec3 lookup_table_color = texture2D(texture_iridescence, vec2(inverse_dot_view, 0.0)).rgb;
 
-   gl_FragColor.rgb = textureCube(texture_reflection, n_reflection).rgb * lookup_table_color * 2.5;
+   gl_FragColor.rgb = textureCube(texture_reflection, n_reflection).rgb * lookup_table_color * 60.5;
    gl_FragColor.a = 1.0;
 }
