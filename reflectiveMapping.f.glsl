96067949aef4f3807244344e65e59a72053d67e5
Jack Nelson <janelson@mymail.mines.edu>
2015-12-15 18:26:51 -0700

    Added back in iridecence, looks more fluid for reflection

--- reflectiveMapping.f.glsl
+++ reflectiveMapping.f.glsl
@@ -1,4 +1,5 @@
 uniform samplerCube texture_reflection;
+uniform sampler2D texture_iridescence;
 uniform sampler2D texture_noise;
 
 varying vec3 v_view_direction;
@@ -16,6 +17,7 @@ void main(void)
    vec3 noise_vector = (texture2D(texture_noise, v_texture_coordinate).xyz - vec3(0.5)) * noise_strength;
 
    float inverse_dot_view = 1.0 - max(dot(normalize(n_normal + noise_vector), n_wiew_direction), 0.0);
+   vec3 lookup_table_color = texture2D(texture_iridescence, vec2(inverse_dot_view, 0.0)).rgb;
 
    gl_FragColor.rgb = textureCube(texture_reflection, n_reflection).rgb * lookup_table_color * 2.5;
    gl_FragColor.a = 1.0;
